eb075e4d1116823f2e631d44dce47aee7287bcc2
xtreak <tir.karthi@gmail.com>
2017-11-25 08:32:45 +0000

    Add wordcloud, database and whole lot of goodies

--- README.md
+++ README.md
@@ -2,13 +2,37 @@
 
 A simple Reddit bot to caculate comment Karma in Cricket match threads
 
-## Installation 
+## Installation
 
 * Create a virtualenv with `python -m venv cricket-env`
 * Activate the virtualenv with `source cricket-env/bin/activate`
 * `pip install -r requirements.txt`
 * Make a `.env` file in the same directory with environment variables for the script
 * `python cricket-bot.py <MATCH_URL>`
+
+## Top 5 users for the match with the number of comments
+
+```bash
+(cricket-env) ubuntu@localhost:~/match-day-karma-bot$ sqlite3 data.db
+SQLite version 3.11.0 2016-02-15 17:29:24
+Enter ".help" for usage hints.
+sqlite> .mode column
+sqlite> .header on
+sqlite> select count(*) as number_of_comments, username as username, sum(score) as karma from match_7fb2og group by (username) order by sum(score) desc limit 20;
+number_of_comments  username        karma
+------------------  --------------  ----------
+10                  jezfromthebush  411
+10                  xxx-treme       272
+7                   GaryGronk       252
+6                   Bangkok_Dave    211
+15                  CornDogMillion  198
+```
+
+## Wordcloud of comments for 1st Ashes (Day 3 2017)
+
+Use the body column of the table or comments.txt and use this library https://github.com/amueller/word_cloud (Thanks to the @amueller)
+
+![wordcloud](wordcloud.png)
 
 ## LICENSE
 
--- cricket-bot.py
+++ cricket-bot.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
 import sys
+import sqlite3
 
 import praw
 from praw.models import MoreComments
@@ -16,7 +17,7 @@ user_agent='user-agent for /u/YourFavouriteBot'
 
 subreddit = 'Cricket'
 
-def getSubComments(comment, allComments, verbose=True):
+def getSubComments(comment, allComments):
     allComments.append(comment)
     if not hasattr(comment, "replies"):
         replies = comment.comments()
@@ -24,10 +25,9 @@ def getSubComments(comment, allComments, verbose=True):
         replies = comment.replies
 
     for child in replies:
-        getSubComments(child, allComments, verbose=verbose)
+        getSubComments(child, allComments)
 
 if __name__ == "__main__":
-    comments = []
     reddit = praw.Reddit(client_id=client_id,
                          client_secret=client_secret,
                          password=password,
@@ -41,6 +41,14 @@ if __name__ == "__main__":
     url = sys.argv[1]
     scores = defaultdict(int)
     submission = reddit.submission(url=url)
+    submission_id = submission.id
+
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    table_name = 'match_{0}'.format(submission_id)
+    create_table_stmt = 'create table {0} (id integer primary key autoincrement, submission text, comment_id text, username text, score integer, body text);'.format(table_name)
+    insert_table_stmt = 'INSERT INTO {0} VALUES (NULL,?,?,?,?,?)'.format(table_name)
+    c.execute(create_table_stmt)
 
     # Get all comments . https://stackoverflow.com/a/36377995/2610955
     commentsList = []
@@ -49,13 +57,24 @@ if __name__ == "__main__":
 
     i = 0
     # Calculate scores
-    for comment in commentsList:
-        if not isinstance(comment, MoreComments) and comment and comment.author and comment.author.name and comment.score:
-            user = comment.author.name
-            score = comment.score
-            scores[user] += score
-            i += 1
+    filename = 'comments_' + submission.id + '.txt'
+    with open(filename, 'a+') as f:
+        for comment in commentsList:
+            try:
+                if not isinstance(comment, MoreComments) and comment and comment.author and comment.author.name and comment.score is not None and comment.body:
+                    username = comment.author.name
+                    score = comment.score
+                    body = comment.body
+                    scores[username] += score
+                    i += 1
+                    data = [(submission_id, comment.id, username, score, comment.body)]
+                    c.executemany(insert_table_stmt, data)
+                    f.write(comment.body.lower())
+            except Exception as e:
+                print(e)
+                pass # We may fail but we must move on
 
+    conn.commit() # You don't have any data if you don't commit at all :)
     print("Calculated scores from {0} comments".format(i))
     for user, score in sorted(scores.items(), key=lambda x: x[1], reverse=True):
         print("u/{user} {score}".format(user=user, score=score))
